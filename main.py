d3f97893db666f639ba4749d9f544fdc4cdbe0c3
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-02-19 21:24:17 -0800

    Create main.py Command line takes single optional argument to specify the path to the data set.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__": 
-    print("hello world")
+import os
+from argparse import ArgumentParser
+
+def read_json(filepath: str) -> None: 
+    pass 
+
+def parse_content() -> None:
+    pass 
+
+def crawl_data_set(root_dir: str) -> None: 
+    directory_content = os.listdir(root_dir)
+    for dir in directory_content: 
+        print(dir) 
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--filename", type=str, default="developer/Dev")
+    args = parser.parse_args()
+    crawl_data_set(args.filename)
