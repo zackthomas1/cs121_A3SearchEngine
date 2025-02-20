8ab18595fbcac44b9a73b63f706d0b56181e3e96
Min Seop Lee <leems6@uci.edu>
2025-02-20 02:59:53 -0800

    Added interaction with data_loader.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-if __name__ == "__main__": 
-    print("hello world")
+import sys
+import data_loader
+
+
+def run(rootpath: str) -> None:
+    data = data_loader.load_data(rootpath)
+
+    # NOTE: This is code for testing & debugging
+    # TODO: DELETE at least before submission
+    for i, (k, v) in enumerate(data.items()):
+        print(f"{k}, {v}")
+        print(f"KEYS: {k}")
+        print(f"Val's keys: {v.keys()}")
+        if i == 1:
+            break
+
+
+
+if __name__ == "__main__":
+    rootpath = sys.argv[1]  # relative path to root directory name ("/DEV")
+    run(sys.argv[1])
+
+
