7d7afa2d85a5110d162e0a8b14d60f51b187dc9b
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-02-19 21:32:25 -0800

    Create basic top level program structure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 from argparse import ArgumentParser
 
+"""
+These are a few possible functions I thought of just off the top of my head.
+They are not requirements for the project.
+Please replace/delete them if you can think of a better architecture or high level functions.
+"""
+
 def read_json(filepath: str) -> None: 
     pass 
 
@@ -12,6 +18,10 @@ def crawl_data_set(root_dir: str) -> None:
     for dir in directory_content: 
         print(dir) 
 
+"""
+Entry point
+Call 'python main.py' from the command line to run program
+"""
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--filename", type=str, default="developer/Dev")
