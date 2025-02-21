6ce0b81bf64674b14198d5dcfc3c52fd7b3d463f
zackthomas1 <zackthomas13@gmail.com>
2025-02-21 15:37:38 -0800

    Add get_logger function from A2 to util module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,5 @@
+import os
+import logging
 from urllib.parse import urlparse
 
 def clean_url(url: str) -> str:
@@ -7,8 +9,27 @@ def clean_url(url: str) -> str:
     else:
         return normalize_url(parsed_url._replace(query="", fragment="").geturl())
 
+def get_logger(name, filename=None):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+    fh = logging.FileHandler(f"logs/{filename if filename else name}.log")
+    fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+       "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
+
 def normalize_url(url):
     """ strips trailing backslash"""
     if url.endswith("/"):
         return url.rstrip("/")
     return url
+
