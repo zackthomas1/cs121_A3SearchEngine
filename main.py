7beadc613f12df68c137c47df0ea748d21b19eb4
zackthomas1 <zackthomas13@gmail.com>
2025-02-21 13:16:02 -0800

    Implement in memory inverse_index.

--- main.py
+++ main.py
@@ -9,12 +9,32 @@ from nltk.stem import WordNetLemmatizer
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 from collections import Counter
+from urllib.parse import urlparse
 
 # Download NLTK resources
 nltk.download('punkt_tab')
 
+#TODO: These data structures should not be stored in memory. 
+# They need to be stored in a file. The inverse index should be store across multiple files
+docid_url_index : dict[int, str] = {}
+inverse_index : dict[int, list[tuple[str, int]]]  = {}
 
-def construct_token_freq_counter(tokens) -> None:
+def normalize_url(url):
+    if url.endswith("/"):
+        return url.rstrip("/")
+    return url
+
+def update_docid_index(docid : int, url: str) -> None: 
+    docid_url_index[docid] = url
+
+def update_inverse_index(docid: int, token_count: Counter) -> None: 
+    for token, count in token_count.items(): 
+        if token in inverse_index:
+            inverse_index[token].append((docid, count))
+        else: 
+            inverse_index[token] = [(docid, count)]
+
+def construct_token_freq_counter(tokens) -> Counter:
     counter = Counter()
     counter.update(tokens)
     return counter
@@ -33,6 +53,7 @@ def tokenize_text(text: str) -> list[str]:
 def extract_text_from_html_content(content: str) -> list[str]: 
     """
     """
+
     try:
         # Get the text from the html response
         soup = BeautifulSoup(content, 'html.parser')
@@ -65,17 +86,27 @@ def read_json_file(filepath: str) -> dict[str, str]:
     except Exception as e:
         print(f"An unexpected error has orccurred: {e}") 
         return None 
-
+ 
 def walk_directory(rootdir: str) -> None:
     """
-    
     """
     
+    docid = 0
     for root, dirs, files in os.walk(rootdir):
         for file in files:
             data = read_json_file(os.path.join(root, file)) 
             
             if not data: 
+                continue
+
+            url = data['url']
+            parsed_url = urlparse(url)
+            if 'index' in parsed_url.path:
+                clean_url = normalize_url(parsed_url._replace(path="", query="", fragment="").geturl())
+            else:
+                clean_url = normalize_url(parsed_url._replace(query="", fragment="").geturl())
+
+            if clean_url in set(docid_url_index.values()):
                 continue
 
             text = extract_text_from_html_content(data['content'])
@@ -84,10 +115,12 @@ def walk_directory(rootdir: str) -> None:
                 continue
             
             tokens = stem_tokens(tokenize_text(text))
+            token_count = construct_token_freq_counter(tokens)
+            
+            update_inverse_index(docid, token_count)
+            update_docid_index(docid, clean_url)
 
-            token_freq = construct_token_freq_counter(tokens)
-            print(token_freq)
-
+            docid += 1
 """
 Entry point
 Call 'python main.py' from the command line to run program
