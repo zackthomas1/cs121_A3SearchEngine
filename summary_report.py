99731bf0009ab74041dfa0e85d8534ee12030665
zackthomas1 <zackthomas13@gmail.com>
2025-02-21 14:38:36 -0800

    Rename query_index.py to summary_report.py

---
+++ summary_report.py
@@ -0,0 +1,9 @@
+# Code Report: 
+# A table with assorted numbers pertaining to your index. 
+# Contains:
+#   Number of documents
+#   Number of [unique] tokens
+#   Total size (in KB) of index on disk.
+
+if __name__ == "__main__":
+    pass
