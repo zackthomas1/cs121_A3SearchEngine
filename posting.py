da5e231075070b71713f4168fed5c7453af4d1b5
Min Seop Lee <leems6@uci.edu>
2025-02-21 05:21:45 -0800

    Updated __init__ and comments

--- posting.py
+++ posting.py
@@ -4,13 +4,22 @@ from typing import List, Tuple
 class Posting:
     """
     Posting that represents {Document ID: frequency of token in that document}
+    Used in Indexer class as following: {token: Posting}
     """
-    def __init__(self):
-        self.posting = dict()
-        self.size = 0
+    def __init__(self, id: int = None, freq: int = None):
+        """
+        If id and freq are provided, initializes with one entry; otherwise, empty.
+        """
+        if id is not None and freq is not None:
+            self.posting = {id: freq}
+            self.size = 1
+        else:
+            self.posting = {}  # Initialize empty dictionary
+            self.size = 0
+        # NOTE: we can add self.token that saves corresponding token if needed (consider memeory problem)
 
 
-    def add(self, id, freq) -> None:
+    def add(self, id: int , freq: int) -> None:
         if id not in self.posting:
             self.posting[id] = freq
         else:
@@ -23,7 +32,7 @@ class Posting:
         return sorted(self.posting.items()) if sorted else list(self.posting.items())
 
 
-    def get_freq(self, id) -> int:
+    def get_freq(self, id: int) -> int:
         return self.posting[id] if id in self.posting else -1
     
 
