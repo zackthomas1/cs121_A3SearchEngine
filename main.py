10dba95f48fa866f78b5ee33ca3844d347dab2e6
zackthomas1 <zackthomas13@gmail.com>
2025-02-21 11:36:49 -0800

    Read json files and extract text from html content

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os
+import json
+import logging
+import shelve
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 from argparse import ArgumentParser
+from bs4 import BeautifulSoup
+
+# Download NLTK resources
+nltk.download('punkt_tab')
 
 """
 These are a few possible functions I thought of just off the top of my head.
@@ -10,20 +20,66 @@ Please replace/delete them if you can think of a better architecture or high lev
 def stemm_tokens(tokens: list[str]) -> list[str]: 
     pass
 
-def tokenize_content(content: str) -> list[str]: 
-    pass 
+def tokenize_text(text: str) -> list[str]: 
+    return word_tokenize(text)
 
-def parse_json_file(filename: str) -> str:
-    pass 
+def extract_text_from_html_content(content: str) -> list[str]: 
+    """
+    """
+    try:
+        # Get the text from the html response
+        soup = BeautifulSoup(content, 'html.parser')
 
-def walk_directory(root_dir: str) -> None: 
-    # directory_content = os.walk(root_dir)
-    for root, dirs, files in os.walk(root_dir):
-        for dir in dirs: 
-            walk_directory(dir)
+        # Remove the text of CSS, JS, metadata, alter for JS, embeded websites
+        for markup in soup.find_all(["style", "script", "meta", "noscript", "iframe"]):  
+            markup.decompose()  # remove all markups stated above
+        
+        # soup contains only human-readable texts now to be compared near-duplicate
+        text = soup.get_text(separator=" ", strip=True)
+        return text
+    except Exception as e:
+        print(f"An unexpected error has orccurred: {e}") 
+        return None 
+        
+def read_json_file(filepath: str) -> dict[str, str]:
+    """
+    """
 
+    try: 
+        with open(filepath, 'r') as file: 
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"File note found at path: {filepath}")
+        return None 
+    except json.JSONDecodeError: 
+        print(f"Invalid JSON format in file:  {filepath}")
+        return None 
+    except Exception as e:
+        print(f"An unexpected error has orccurred: {e}") 
+        return None 
+
+
+def walk_directory(rootdir: str) -> None:
+    """
+    
+    """
+    
+    for root, dirs, files in os.walk(rootdir):
         for file in files:
-            parse_json_file(file) 
+            data = read_json_file(os.path.join(root, file)) 
+            
+            if not data: 
+                continue
+
+            text = extract_text_from_html_content(data['content'])
+
+            if not text: 
+                continue
+            
+            tokens = tokenize_text(text)
+
+
 
 """
 Entry point
@@ -31,6 +87,7 @@ Call 'python main.py' from the command line to run program
 """
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--filename", type=str, default="developer")
+    parser.add_argument("--rootdir", type=str, default="developer\DEV")
     args = parser.parse_args()
-    walk_directory(args.filename)
+
+    walk_directory(args.rootdir)
