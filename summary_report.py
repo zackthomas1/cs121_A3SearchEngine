a9cd65a37321063206de76574eb82624da4d1f55
zackthomas1 <zackthomas13@gmail.com>
2025-02-21 16:00:58 -0800

    Implement print_inverse_index method to out inverse dictionary. Implement list_urls to get list of all indexed urls.

--- summary_report.py
+++ summary_report.py
@@ -1,3 +1,5 @@
+import shelve
+
 # Code Report: 
 # A table with assorted numbers pertaining to your index. 
 # Contains:
@@ -5,5 +7,18 @@
 #   Number of [unique] tokens
 #   Total size (in KB) of index on disk.
 
+def print_inverse_index(inverse_index_filepath: str) -> None: 
+    with shelve.open(inverse_index_filepath) as db:
+        for token, doc_freq in db.items(): 
+            print(f"{token} - ", end=" ")
+            for docid, freq in doc_freq:
+                print(f"({docid}, {freq})", end= " ")
+            print()
+
+def list_urls(docid_index_filepath: str) -> None: 
+    with shelve.open(docid_index_filepath) as db:
+        return list(db.values())
+
 if __name__ == "__main__":
-    pass
+    print_inverse_index('shelve/inverse_index.shelve')
+    # list_urls()
