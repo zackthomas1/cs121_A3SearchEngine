8759e1f741e7e4aadd63c588fad545a603f5e2dd
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2025-02-22 18:34:01 -0800

    Add check for URLs with common non-html extensions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+import re
 import os
 import logging
 from urllib.parse import urlparse
@@ -8,6 +9,21 @@ def clean_url(url: str) -> str:
         return normalize_url(parsed_url._replace(path="", query="", fragment="").geturl())
     else:
         return normalize_url(parsed_url._replace(query="", fragment="").geturl())
+
+def is_non_html_extension(url: str) -> bool: 
+    parsed_url = urlparse(url)
+
+    return re.match(
+    r".*\.(css|js|bmp|gif|jpe?g|ico"
+    + r"|png|tiff?|mid|mp2|mp3|mp4"
+    + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+    + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+    + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+    + r"|epub|dll|cnf|tgz|sha1"
+    + r"|thmx|mso|arff|rtf|jar|csv"
+    + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
+    + r"|img|java|war|sql|mpg|ff|sh|ppsx|py|apk|svg|conf|cpp|fig|cls|ipynb|bam|odp|odc|tsv|nb|bib|z|rpm|ma)$", parsed_url.path.lower())
+
 
 def get_logger(name, filename=None):
     logger = logging.getLogger(name)
