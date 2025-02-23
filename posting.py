82811ce1e14902ccf0f5e8fc3e969554db671860
Min Seop Lee <leems6@uci.edu>
2025-02-23 06:50:31 -0800

    Added function to import/export Posting data

--- posting.py
+++ posting.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 
 class Posting:
@@ -19,6 +19,13 @@ class Posting:
         # NOTE: we can add self.token that saves corresponding token if needed (consider memeory problem)
 
 
+    def __str__(self):
+        output = []
+        for tok, post in self.posting.items():
+            output.append(f"    Doc {tok} - Freq {post}")
+        return "\n".join(output)
+
+
     def add(self, id: int , freq: int) -> None:
         if id not in self.posting:
             self.posting[id] = freq
@@ -28,8 +35,31 @@ class Posting:
         self.size += 1
 
 
-    def get_posting(self, sorted=True) -> List[Tuple[int, int]]:
-        return sorted(self.posting.items()) if sorted else list(self.posting.items())
+    def get_posting(self, sorting=True) -> List[Tuple[int, int]]:
+        """
+        Exports data by returning posting in dictionary (choose sorted/unsorted)
+        """
+        return sorted(self.posting.items()) if sorting else list(self.posting.items())
+    
+
+    def update_posting(self, data: Dict[str, int]) -> None:
+        """
+        Updates current Posting object's data
+        """
+        for id, frq in data.items():
+            self.add(int(id), frq)
+        self.size += len(data)
+
+
+    @classmethod
+    def convert_to_posting_object(cls, data: Dict[str, int]) -> "Posting":
+        """
+        Create new Posting object and returns it with data filled
+        """
+        postingObj = cls()  # Create new Posting object
+        postingObj.posting = {int(id): frq for id, frq in data.items()}
+        postingObj.size = len(data)
+        return postingObj   # Return object with data filled
 
 
     def get_freq(self, id: int) -> int:
@@ -38,4 +68,11 @@ class Posting:
 
     def get_size(self) -> int:
         return self.size
+    
+
+    def clear_all(self) -> None:
+        """
+        Clear all posting dictionary data
+        """
+        self.posting.clear()
     
