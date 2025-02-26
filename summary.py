b0f3d71a0e88513a4d059d8d2e5a4e9f16136e63
Brandon Chan <chanbz@uci.edu>
2025-02-26 00:38:21 -0800

    Finalized m2 report implementation

--- summary.py
+++ summary.py
@@ -3,6 +3,7 @@ import json
 from inverted_index import InvertedIndex
 from inverted_index import PARTIAL_INDEX_DIR, MASTER_INDEX_DIR, MASTER_INDEX_FILE, DOC_ID_DIR, DOC_ID_MAP_FILE
 from typing import List
+from utils import get_logger
 # Code Report: 
 # A table with assorted numbers pertaining to your index. 
 # Contains:
@@ -42,8 +43,7 @@ def unique_tokens_count():
 
     return len(list(unique_tokens))
 
-#TODO: WIP
-def top_n_result_urls(query: str, n: int) -> List[str]:
+def top_n_result_urls(query: str, n: int, index: InvertedIndex, logger) -> List[str]:
     """
     Given a query, pulls and returns the top n Urls for each from an already
     built inverted index.
@@ -55,14 +55,14 @@ def top_n_result_urls(query: str, n: int) -> List[str]:
     
     {'ahm': [[111, 1], [238, 2], [499, 1], [4360, 1], [5006, 4], [592, 3], [686, 1], [744, 1], [745, 1], [5013, 2], [5030, 2], [5035, 2], [5038, 2], [5047, 6], [5051, 2], [5095, 2], [5118, 2], [5138, 2], [835, 1], [1110, 1], [1133, 1]]}
     """
-    index = InvertedIndex()
+    logger.info(f"Processing query: {query}")
     relevent_index = index.search(query)
-
     # merged_results = {docId: token_occurances}
     merged_results = {}
 
     # AND boolean implementation: merge docId results on token occurances
     for token in relevent_index:
+        logger.info(f"Processing token: {token}")
         # Initialize 'merged_results' if empty
         if not merged_results:
             merged_results = {key: value for key, value in relevent_index[token]}
@@ -82,16 +82,23 @@ def top_n_result_urls(query: str, n: int) -> List[str]:
     # Get the top n URLS
     id_map = {}
 
+    # Load the docId map to get URLS
     if os.path.exists(DOC_ID_MAP_FILE):
         with open(DOC_ID_MAP_FILE, "r", encoding="utf-8") as f: 
             id_map = json.load(f)
 
-    ordered_url_list = []
-    for i in range(n):
-        doc_id = merged_results[i][0]
-        ordered_url_list.append(id_map[doc_id])
+    # Get and return the top N urls
+    ordered_url_list = set()
 
-    return ordered_url_list
+    num_index = 0
+    while len(ordered_url_list) < n:
+        if num_index > len(merged_results):
+            break
+        doc_id = merged_results[num_index][0]
+        ordered_url_list.add(id_map[str(doc_id)])
+        num_index += 1
+
+    return list(ordered_url_list)
 
 
 def print_token_data():
@@ -103,22 +110,24 @@ def print_token_data():
 
 def print_m2_report():
     """
-    Processes and prints the m2 requirements to the console
+    Processes and writes the m2 requirements to report2.txt
     """
     N_RESULTS = 5
     queries = ["cristina lopes",
                "machine learning",
                "ACM",
                "master of software engineering"]
-    
-    print(f"Top {N_RESULTS} Query Results")
-    for query in queries:
-        print(query)
-        top_query_results = top_n_result_urls(N_RESULTS)
-        count = 1
-        for url in top_query_results:
-            print(f"\t{count}. {url}")
-            count += 1
+    index = InvertedIndex()
+    logger = get_logger("TOP_N_RESULTS")
+
+    with open("report2.txt", "w", encoding="utf-8") as file:
+        file.write(f"Top {N_RESULTS} Query Results\n")
+        for query in queries:
+            file.write(f"{query}\n")
+            top_query_results = top_n_result_urls(query, N_RESULTS, index, logger)
+            for count, url in enumerate(top_query_results, start=1):
+                file.write(f"\t{count}. {url}\n")
+
 
 if __name__ == "__main__":
     #index = InvertedIndex()
