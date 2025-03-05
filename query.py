3d6877883b620c8a39c355bf365596633faad4ba
zackthomas1 <zackthomas13@gmail.com>
2025-03-05 11:40:46 -0800

    Move main method to main.py. Add compute_ft_idf ranked_boolean_search and tokenize_query methods to query.py file.

--- query.py
+++ query.py
@@ -1,38 +1,54 @@
-import os
-import json
-import time
 from inverted_index import InvertedIndex
-from inverted_index import PARTIAL_INDEX_DIR, MASTER_INDEX_DIR, MASTER_INDEX_FILE, DOC_ID_DIR, DOC_ID_MAP_FILE
-from typing import List
-from utils import get_logger
-from summary import top_n_result_urls
+from utils import tokenize_text, stem_tokens
 
 
-def form_query_tokens(query: str) -> list[str]: 
-    pass
+def tokenize_query(query: str) -> list[str]: 
+    return stem_tokens(tokenize_text(query))
 
-if __name__ == "__main__":
-    RESULT_NUM = 5
+def ranked_boolean_search(query_tokens: list[str], inverted_index: InvertedIndex) -> list[set[str, int]]:
+    """
+    Parameters:
+    query_tokens (list[str]): a query string 
 
-    index = InvertedIndex()
-    logger = get_logger("SEARCHER")
-    input_text = ""
-    while (input_text != "quit"):
-        print("Please enter the query you'd like to switch (or type 'quit' to exit)")
-        input_text = input()
+    Returns:
+    dict[str, list[tuple[int, int]]]: Inverted index containing only tokens formed from the query string
+    """
+   
+    merged_index = inverted_index.construct_merged_index_from_disk(query_tokens)
 
-        if (input_text != "quit"):
-            print(f'Searching for "{input_text}"')
+    scores = {}
+    # AND boolean implementation: merge docId results on token occurances
+    for token in query_tokens:
+        # Initialize 'merged_results' if empty
+        if not scores:
+            scores = {doc_id: freq for doc_id, freq, tf in merged_index[token]}
 
-            # Begin timing after recieving search query
-            start_time = time.perf_counter() * 1000
+        # Find and merge relevent documents
+        else:
+            relevent_documents = merged_index[token]
 
-            logger.info(f'Searching "{input_text}"')
-            results = top_n_result_urls(input_text, RESULT_NUM, index, logger)
-            for count, url in enumerate(results, start=1):
-                print(f"{count}: {url}")
-            end_time = time.perf_counter() * 1000
-            logger.info(f"Completed search: {end_time - start_time:.0f} ms")
+            for docId, token_freq in relevent_documents:
+                if docId in scores:
+                    scores[docId] += token_freq
 
-    print("'quit' detected, exiting...")
-    logger.info("User ended searching.")
+    # Sort the merged results by their "quality" [# of token frequency]
+    return sorted(scores.items(), key=lambda kv: (-kv[1], kv[0]))
+
+    def compute_tf_idf(token: str, doc_id: int) -> int:
+        """
+        Call this function in loop for all documents like below.
+            tfidf_table = 
+            For text[contents] in all document files:
+                for each token in each text[contents]:
+                    tf = __calculate_tfscore(token, text)
+
+        """
+        term_freq = 0
+        doc_freq = 0 
+        doc_length = 0
+        tf = term_freq /  doc_length 
+        total_docs = 0
+        idf = math.log(total_docs / (1+doc_freq))
+
+        return tf * idf
+
