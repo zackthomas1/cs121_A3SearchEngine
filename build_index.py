9514dc3287db1f73348a484e5177b2efdc285efa
zackthomas1 <zackthomas13@gmail.com>
2025-03-05 23:40:27 -0800

    Move downloading ntlk resources/libraries from build_index.py to setup.py file. setup.py should be run first.

--- build_index.py
+++ build_index.py
@@ -1,9 +1,7 @@
-import nltk
 from argparse import ArgumentParser
 from inverted_index import InvertedIndex
 
 """
-Entry point
 Call 'python build_index.py' from the command line to construct inverted index
 """
 if __name__ == "__main__":
@@ -12,9 +10,6 @@ if __name__ == "__main__":
     parser.add_argument("--restart", action="store_true", default=False)
     args = parser.parse_args()
     
-    # Download NLTK resources
-    nltk.download('punkt_tab')
-
     index = InvertedIndex()
     index.build_index(args.rootdir)
     index.build_master_index()
